b7b6f57b08ef85869e9162d303cfe3db493509a9
Rodolfo <rodolfo_24@live.com.mx>
2018-03-06 14:32:32 -0600

    Agregando funcionalidad de modificar artista, eliminar artista y agregar imagen al artista

--- controllers/artist.js
+++ controllers/artist.js
@@ -86,6 +86,79 @@ function updateArtist(req,res){
   });
 }
 
+function deleteArtist(req,res){
+  var artistId = req.params.id;
+  Artist.findByIdAndRemove(artistId,(err,artistRemoved)=>{
+    if(err){
+      res.status(500).send({message:"Error al eliminar el artista"});
+    }else{
+      if(!artistRemoved){
+        res.status(404).send({message:"El artista no ha sido eliminado"});
+      }else{
+        Album.find({artist:artistRemoved._id}).remove((err,albumRemoved)=>{
+            if(err){
+              res.status(500).send({message:"Error al eliminar el album"});
+            }else{
+              if(!albumRemoved){
+                res.status(404).send({message:"El album no ha sido eliminado"});
+              }else{
+                  Song.find({album:albumRemoved._id}).remove((err,songRemoved)=>{
+                      if(err){
+                        res.status(500).send({message:"Error al eliminar la cancion"});
+                      }else{
+                        if(!songRemoved){
+                          res.status(404).send({message:"La cancion no ha sido eliminado"});
+                        }else{
+                          res.status(200).send({artist:artistRemoved});
+                        }
+                      }
+                  });
+              }
+            }
+        });
+      }
+    }
+  });
+}
+
+function uploadImage(req,res){
+  var artistId = req.params.id;
+  var file_name = "No subido...";
+  if(req.files){
+    var file_path = req.files.image.path;
+    var file_split = file_path.split('\\');
+    var file_name = file_split[2];
+    var ext_split = file_name.split('\.');
+    var file_ext = ext_split[1];
+
+    if(file_ext == 'png'||file_ext == 'jpg'||file_ext == 'gif'){
+      Artist.findByIdAndUpdate(artistId,{image:file_name},(err,artistUpdated)=>{
+        if(!artistUpdated){
+          res.status(404).send({message: "No se ha podido actualizar el artista"});
+        }else{
+          res.status(200).send({artist:artistUpdated});
+        }
+      });
+    }else{
+      res.status(200).send({message: "Extension del archivo no valida"});
+    }
+  }else{
+    res.status(200).send({message: "No has subido una imagen"});
+  }
+}
+
+function getImageFile(req,res){
+  var imageFile = req.params.imageFile;
+  var path_file= './uploads/artists/'+imageFile;
+  fs.exists(path_file,function(exists){
+    if(exists){
+      res.sendFile(path.resolve(path_file));
+    }else{
+      res.status(200).send({message: "No existe la imagen..."});
+    }
+  });
+}
+
 module.exports={
-  getArtist,saveArtist,getArtists,updateArtist
+  getArtist,saveArtist,getArtists,updateArtist,deleteArtist,uploadImage,getImageFile
 };
