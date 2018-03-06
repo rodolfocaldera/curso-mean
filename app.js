c95fe3ddac6beeea0de2d5c2b5c0df614806205d
Rodolfo <rodolfo_24@live.com.mx>
2018-03-06 16:08:45 -0600

    Controlador de albums

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var app = express();
 //cargar rutas
 var user_routes = require('./routes/user');
 var artist_routes = require('./routes/artist');
+var album_routes = require('./routes/album');
 app.use(bodyParse.urlencoded({extendes:false}));
 app.use(bodyParse.json());
 
@@ -16,4 +17,5 @@ app.use(bodyParse.json());
 //Rutas base
 app.use('/api',user_routes);
 app.use('/api',artist_routes);
+app.use('/api',album_routes);
 module.exports = app;
