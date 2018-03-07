c6c24d98f354d7b0f08dcb145ae891a07682d83e
Rodolfo <rodolfo_24@live.com.mx>
2018-03-06 18:41:11 -0600

    Controlador song y configuracion de cabeceras

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ var app = express();
 var user_routes = require('./routes/user');
 var artist_routes = require('./routes/artist');
 var album_routes = require('./routes/album');
+var song_routes = require('./routes/song');
 app.use(bodyParse.urlencoded({extendes:false}));
 app.use(bodyParse.json());
 
 //Configurar cabeceras http
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Origin','*');
+  res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY,Origin,X-Requested-With,Content-Type,Accept,Access-Control-Allow-Request-Method');
+  res.header('Access-Allow-Methods','GET,POST,OPTIONS,PUT,DELETE');
+  res.header('Allow','GET,POST,OPTIONS,PUT,DELETE');
 
+  next();
+});
 
 //Rutas base
 app.use('/api',user_routes);
 app.use('/api',artist_routes);
 app.use('/api',album_routes);
+app.use('/api',song_routes);
 module.exports = app;
