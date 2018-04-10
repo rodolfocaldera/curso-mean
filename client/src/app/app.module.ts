7ed064c868901724e2f7a9ae28fb9939d247f191
rodolfocaldera <rodolfo_24@live.com.mx>
2018-04-10 15:01:57 -0500

    Añadiendo el player

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,26 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {routing,appRoutingProviders} from "./app.routing";
 import { AppComponent } from './app.component';
+//User
 import {UserEditComponent} from "./components/user-edit.component";
+
+//Artist
 import {ArtistListComponent} from "./components/artist-list.component";
 import {ArtistAddComponent} from "./components/artist-add.component";
 import {ArtistEditComponent} from "./components/artist-edit.component";
 import {ArtistDetailComponent} from "./components/artist-detail.component";
+
+//álbum
 import {AlbumAddComponent} from "./components/album-add.component";
 import {AlbumEditComponent} from "./components/album-edit.component";
 import {AlbumDetailComponent} from "./components/album-detail.component";
+
+//Song
 import {SongAddComponent} from "./components/song-add.component";
 import {SongEditComponent} from "./components/song-edit.component";
+import {PlayerComponent} from "./components/player.component";
+
+//Home
 import {HomeComponent} from "./components/home.component";
 
 @NgModule({
@@ -29,7 +39,8 @@ import {HomeComponent} from "./components/home.component";
     AlbumEditComponent,
     AlbumDetailComponent,
     SongAddComponent,
-    SongEditComponent
+    SongEditComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/components/player.component.ts
@@ -0,0 +1,26 @@
+import {Component,OnInit} from '@angular/core';
+import {Song} from "../models/song";
+import {GLOBAL} from "../services/global";
+
+@Component({
+    selector: "player",
+    templateUrl:"../views/player.html",
+})
+
+export class PlayerComponent implements OnInit{
+    public url : string;
+    public song;
+
+    constructor(){
+        this.url=GLOBAL.url;
+    }
+    ngOnInit(){
+        console.log("player cargado");
+        var song = JSON.parse(localStorage.getItem('sound_song'));
+        if(song){
+            this.song=song;
+        }else{
+            this.song = new Song(1,"","","","");
+        }
+    }
+}
